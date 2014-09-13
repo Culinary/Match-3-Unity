d7081c8c1f9b2952c9e7cbda210c0395f7e32fe4
Culinary <brett61188@gmail.com>
2014-09-13 11:53:30 -0400

    Initial Commit
    Commits all basic hierarchy and game objects

---
+++ Project_Dolphin_2/Assets/Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour {
+
+	public List<Gem> gems = new List<Gem>();
+	public GameObject gemPrefab;
+	public int GridWidth;
+	public int GridHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int y=0;y<GridHeight;y++)
+		{
+			for(int x=0;x<GridWidth;x++)
+			{
+				GameObject g = Instantiate (gemPrefab,new Vector3(x,y,0),Quaternion.identity)as GameObject;
+				g.transform.parent = gameObject.transform;
+				gems.Add(g.GetComponent<Gem>());
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
